c44db0de08ca56e208d295c47123f58a4fbd9991
Kaleb <kaleb.m.tefera@gmail.com>
2022-05-22 19:44:25 +0300

    added horizontal,vertical, and block solver

--- final.py
+++ final.py
@@ -2,14 +2,28 @@ import pygame as pg
 
 pg.init()
 FONT = pg.font.Font(None,35)
+FONTpossy = pg.font.Font(None,20)
+
 black = pg.Color('black')
+lightblue = pg.Color(30,125,150)
+grey = pg.Color(100,100,100)
 FPS = 30
-window = pg.display.set_mode((450,450))
+window = pg.display.set_mode((500,450))
 xcounter = -1
 ycounter = 0
 board = []
 lists2 =[]
 
+def drawingPossy(window, text,x, y, line):  
+    if line == 1:    
+        textsurface = FONTpossy.render(text, True, grey)
+        window.blit(textsurface,(x+3,y+4))
+    elif line == 2:
+        textsurface = FONTpossy.render(text, True, grey)
+        window.blit(textsurface,(x+3,y+18))
+    elif line == 3:
+        textsurface = FONTpossy.render(text, True, grey)
+        window.blit(textsurface,(x+3,y+35))
 
 class highlightbox:
     def __init__(self,x,y,w,h) -> None:
@@ -21,19 +35,21 @@ class highlightbox:
         pg.draw.rect(window, self.activeColor,self.recta,2)
 
 class box:
-    def __init__(self,x,y,w,h,num) -> None:
+    def __init__(self,x,y,w,h,num,textcolor) -> None:
         self.recta = pg.Rect(x,y,w,h)
         self.width = 1
         self.x = x
         self.y = y 
         self.num = num
         self.active = False
+        self.textcolor = textcolor
         self.activeColor = pg.Color("black")
-        self.textsurface = FONT.render(self.num, True, black)
+        self.textsurface = FONT.render(self.num, True, self.textcolor)
     
-    def draw(self, window):
+    def draw(self,window):
         pg.draw.rect(window, self.activeColor,self.recta,self.width)
         window.blit(self.textsurface,(self.x+5,self.y+5))
+
     def inputs(self, event):
         if pg.MOUSEBUTTONDOWN == event.type:
             if self.recta.collidepoint(event.pos):
@@ -55,27 +71,17 @@ class box:
                         pass
                     else:
                         self.num += event.unicode
-        self.textsurface = FONT.render(self.num,True,black)
+        self.textsurface = FONT.render(self.num,True,self.textcolor)
 
 
 class algorithm:
     def __init__(self) -> None:
         pass
-    def row(self, numrow):
-        row = []
-        for x in range(8):
-            row.append(board[numrow][x])
-        return row
-
-    def column(self, numcolumn):
-        column =[]
-        for x in range(8):
-            column.append(board[x][numcolumn])
-        return column
     def block(self, numblock):
         # 123
         # 456
         # 789
+        global fx,fy
         if numblock == 1:
             fx, fy = 0,0
         elif numblock == 2:
@@ -88,7 +94,7 @@ class algorithm:
             fx, fy = 3,3
         elif numblock == 6:
             fx, fy = 6,3
-        elif numblock == 1:
+        elif numblock == 7:
             fx, fy = 0,6
         elif numblock == 8:
             fx, fy = 3,6
@@ -96,58 +102,68 @@ class algorithm:
             fx, fy = 6,6   
         else:
             print("ERROR ON BLOCK")
-        solid = []
-        for y in range(3):
-            for x in range(3):
-                solid.append((board[y+fy][x+fx]))
-        return solid
-    def possibilities():
+       
+    def setpossibilities(self):
         global lists2
-        for x in range(2):
+        for z in range(2):
             lists2.append([])
             for y in range(9):
-                lists2[x].append([])
-                for z in range(9):
-                    lists2[x][y].append([])
-        for x in range (9):
-            for y in range(9):
+                lists2[z].append([])
+                for x in range(9):
+                    lists2[z][y].append([])
+            
+        for y in range (9):
+            for x in range(9):
                 lists2[0][y][x] = board[y][x]
-        for x in range (9):
-            for y in range(9):
+        for y in range (9):
+            for x in range(9):
                 lists2[1][y][x] = [1,2,3,4,5,6,7,8,9]
-        for x in range (9):
-            for y in range(9):
-                if board[x][y] == 0:
+        for y in range (9):
+            for x in range(9):
+                if board[y][x] != 0:
                     lists2[1][y][x] = [-1]
-    def horizontalcheck(self):
-        for x in range(9):
-            for y in range(9):
+    def checkremove(self):
+        #horizontal
+        for y in range(9):
+            for x in range(9):
+                    if board[y][x] != 0:
+                        for xchange in range(9):
+                            if lists2[1][y][xchange].__contains__(board[y][x]):
+                                lists2[1][y][xchange].remove(board[y][x])
+        #vertical
+        for y in range(9):
+            for x in range(9):
                 if board[y][x] != 0:
                     for ychange in range(9):
-                        lists2[1][ychange][x].remove(board[y][x])
-    def verticalcheck(self):
-        for x in range(9):
-            for y in range(9):
-                if board[y][x] != 0:
-                    lists2[1][y][x].remove(board[y][x])
-    def blockcheck(self):
-
-        pass
-
+                        if lists2[1][ychange][x].__contains__(board[y][x]):
+                            lists2[1][ychange][x].remove(board[y][x])
+        #block
+        for blockno in range(1,10):
+            self.block(blockno)
+            for y in range(3):
+                for x in range(3):
+                    for ychange in range(3):
+                        for xchange in range(3):
+                            if lists2[1][ychange+fy][xchange+fx].__contains__(board[y+fy][x+fx]):
+                                lists2[1][ychange+fy][xchange+fx].remove(board[y+fy][x+fx])
+        
+    
 def num():
     global xcounter
     global ycounter
     global board
     board = [
-    [0,0,7,0,4,0,0,0,0],
-    [0,0,0,0,0,8,0,0,6],
-    [0,4,1,0,0,0,9,0,0],
-    [0,0,0,0,0,0,1,7,0],
-    [0,0,0,0,0,6,0,0,0],
-    [0,0,8,7,0,0,2,0,0],
-    [3,0,0,0,0,0,0,0,0],
-    [0,0,0,1,2,0,0,0,0],
-    [8,6,0,0,7,6,0,0,5],
+    [0,9,4, 0,3,0, 1,0,0],
+    [8,1,2, 7,0,0, 0,9,6],
+    [3,0,0, 1,9,0, 0,0,0],
+
+    [0,3,0, 9,0,4, 6,0,0],
+    [0,0,8, 6,1,3, 0,4,9],
+    [0,0,6, 2,0,0, 0,0,1],
+
+    [4,0,3, 5,0,0, 0,0,8],
+    [5,0,0, 0,2,0, 7,0,0],
+    [0,6,0, 0,0,8, 4,1,5]
     ]
     xcounter = xcounter +1
     if xcounter == 9:
@@ -166,16 +182,44 @@ def main():
     highlight_boxes = []
     boxes = []
     run = True
-    for x in range(0,450,50):
-        for y in range(0,450,50):
-            box1 = box(y,x,50,50,str(num()))
+
+    for y in range(0,450,50):
+        for x in range(0,450,50):
+            box1 = box(x,y,50,50,str(num()), black)
             boxes.append(box1)
     
     for x in range(0,450,150):
         for y in range(0,450,150):
             box1 = highlightbox(x,y,150,150)
             highlight_boxes.append(box1)
-          
+    algo = algorithm()
+    algo.setpossibilities()
+    algo.checkremove()
+    algo.checkremove()
+    algo.checkremove()
+    
+    counter = 0
+    anotherlist = []
+    listsfinal = []
+    for y in range(9):
+        for x in range(9):
+            if lists2[1][y][x].__len__() == 1:
+                if lists2[1][y][x] == [-1]:
+                    continue
+                print(str(lists2[1][y][x])+" "+str(y) +" "+str(x))
+                counter += 1
+                for s in lists2[1][y][x]:
+                    a = str(s)
+                anotherlist.append([x, y, a])
+    print(anotherlist)
+
+    for y in range(counter):
+        da = box(anotherlist[y][0] * 50, anotherlist[y][1] * 50, 50, 50, anotherlist[y][2], lightblue)
+        listsfinal.append(da)
+
+
+
+
     while run:
         for event in pg.event.get():
             if event.type == pg.QUIT:
@@ -189,7 +233,24 @@ def main():
 
         for rectan in highlight_boxes:
             rectan.draw(window)
+
+        for rectan in listsfinal:
+            rectan.draw(window)
+        """
+        tempo = ""
+        for y in range(9):
+            for x in range(9):
+                if lists2[1][y][x].__len__() != 1:
+                    if lists2[1][y][x].__contains__(1):
+                            for r in lists2[1][y][x]:
+                                tempo += str(r)
+                drawingPossy(window, tempo, x * 50, y * 50,1)
+
+
+        """
         pg.display.flip()
+
         clock.tick(FPS)
     pg.quit()
 main()
+lists2.co
--- hh.py
+++ hh.py
@@ -1,11 +1,145 @@
-newboard =[]
-for y in range(9):
-    newboard.append([])
-    for x in range(9):
-        newboard[y].append([])
-print(newboard)
+import numpy as np
+lists2 =[]
+board = [
+    [0,9,4, 0,3,0, 1,0,0],
+    [8,1,2, 7,0,0, 0,9,6],
+    [3,0,0, 1,9,0, 0,0,0],
 
-[[[], [], [], [], [], [], [], [], []],
- [[], [], [], [], [], [], [], [], []], 
- [[], [], [], [], [], [], [], [], []], 
- [[], [], [], [], [], [], [], [], []], [[], [], [], [], [], [], [], [], []], [[], [], [], [], [], [], [], [], []], [[], [], [], [], [], [], [], [], []], [[], [], [], [], [], [], [], [], []], [[], [], [], [], [], [], [], [], []]]
+    [0,3,0, 9,0,4, 6,0,0],
+    [0,0,8, 6,1,3, 0,4,9],
+    [0,0,6, 2,0,0, 0,0,1],
+
+    [4,0,3, 5,0,0, 0,0,8],
+    [5,0,0, 0,2,0, 7,0,0],
+    [0,6,0, 0,0,8, 4,1,5]
+    ]
+for z in range(2):
+    lists2.append([])
+    for y in range(9):
+        lists2[z].append([])
+        for x in range(9):
+            lists2[z][y].append([])
+    
+for y in range (9):
+    for x in range(9):
+        lists2[0][y][x] = board[y][x]
+for y in range (9):
+    for x in range(9):
+        lists2[1][y][x] = [1,2,3,4,5,6,7,8,9]
+
+for x in range(9):
+            for y in range(9):
+                if board[y][x] != 0:
+                    pass
+"""
+for y in range(8):
+    for x in range(8):
+        if board[y][x] != 0:
+                for xchange in range(9):
+"""
+for y in range(9):
+    for x in range(9):
+            if board[y][x] != 0:
+                for xchange in range(9):
+                    lists2[1][y][xchange].remove(board[y][x])
+def block(self, numblock):
+        # 123
+        # 456
+        # 789
+        if numblock == 1:
+            fx, fy = 0,0
+        elif numblock == 2:
+            fx, fy = 3,0
+        elif numblock == 3:
+            fx, fy = 6,0
+        elif numblock == 4:
+            fx, fy = 0,3
+        elif numblock == 5:
+            fx, fy = 3,3
+        elif numblock == 6:
+            fx, fy = 6,3
+        elif numblock == 1:
+            fx, fy = 0,6
+        elif numblock == 8:
+            fx, fy = 3,6
+        elif numblock == 9:
+            fx, fy = 6,6   
+        else:
+            print("ERROR ON BLOCK")
+        solid = []
+        for y in range(3):
+            for x in range(3):
+                solid.append((board[y+fy][x+fx]))
+        return solid
+            
+x = 1
+y = 0
+for y in range(9):
+    for x in range(9):
+        if board[y][x] != 0:
+            for ychange in range(9):
+                if lists2[1][ychange][x].__contains__(board[y][x]):
+                    lists2[1][ychange][x].remove(board[y][x])
+for y in range (9):
+            for x in range(9):
+                if board[y][x] != 0:
+                    lists2[1][y][x] = [-1]
+fx, fy = -1,-1
+def numblock(num):
+    global fx
+    global fy
+    if num == 1:
+        fx, fy = 0,0
+    elif num == 2:
+        fx, fy = 3,0
+    elif num == 3:
+        fx, fy = 6,0
+    elif num == 4:
+        fx, fy = 0,3
+    elif num == 5:
+        fx, fy = 3,3
+    elif num == 6:
+        fx, fy = 6,3
+    elif num == 1:
+        fx, fy = 0,6
+    elif num == 8:
+        fx, fy = 3,6
+    elif num == 9:
+        fx, fy = 6,6  
+    else:
+        print=("ERROR in block function") 
+for blockno in range(1,10):
+    numblock(blockno)
+    for y in range(3):
+        for x in range(3):
+            for ychange in range(3):
+                for xchange in range(3):
+                    if lists2[1][ychange+fy][xchange+fx].__contains__(board[y+fy][x+fx]):
+                        lists2[1][ychange+fy][xchange+fx].remove(board[y+fy][x+fx])
+
+for y in range(9):
+    for x in range(9):
+        if lists2[1][y][x].__len__() == 1:
+            if lists2[1][y][x] == [-1]:
+                continue
+            print(lists2[1][y][x])
+
+[[[0, 9, 4, 0, 3, 0, 1, 0, 0], 
+[8, 1, 2, 7, 0, 0, 0, 9, 6], 
+[3, 0, 0, 1, 9, 0, 0, 0, 0], 
+[0, 3, 0, 9, 0, 4, 6, 0, 0], 
+[0, 0, 8, 6, 1, 3, 0, 4, 9], 
+[0, 0, 6, 2, 0, 0, 0, 0, 1], 
+[4, 0, 3, 5, 0, 0, 0, 0, 8], 
+[5, 0, 0, 0, 2, 0, 7, 0, 0], 
+[0, 6, 0, 0, 0, 8, 4, 1, 5]],
+
+ [[[6, 7], [-1], [-1], [8], [-1], [2, 5, 6], [-1], [2, 5, 7, 8], [2, 7]], 
+ [[-1], [-1], [-1], [-1], [4, 5], [5], [3, 5], [-1], [-1]], 
+ [[-1], [5, 7], [5, 7], [-1], [-1], [2, 5, 6], [2, 5, 8], [2, 5, 7, 8], [2, 4, 7]], 
+ [[1, 2, 7], [-1], [1, 5, 7], [-1], [5, 7, 8], [-1], [-1], [2, 5, 7, 8], [2, 7]], 
+ [[2, 7], [2, 5, 7], [-1], [-1], [-1], [-1], [2, 5], [-1], [-1]], 
+ [[7, 9], [4, 5, 7], [-1], [-1], [5, 7, 8], [5, 7], [3, 5, 8], [3, 5, 7, 8], [-1]], 
+ [[-1], [2, 7], [-1], [-1], [6, 7], [1, 6, 7, 9], [2, 9], [2, 6], [-1]], 
+ [[-1], [4, 8], [1, 9], [3, 4], [-1], [1, 6, 9], [-1], [3, 6], [3]], 
+ [[2, 7, 9], [-1], [7, 9], [3], [7], [-1], [-1], [-1], [-1]]]]
